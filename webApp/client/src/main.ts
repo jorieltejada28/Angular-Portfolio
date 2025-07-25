b3b844feef20199e739472d79e89fa500c13e108
jorieltejada28 <jorieltejada84@gmail.com>
2025-07-25 23:27:54 +0800

    commit

--- webApp/client/src/main.ts
+++ webApp/client/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations'; 
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideNoopAnimations(),
+    ...(appConfig.providers || [])
+  ]
+}).catch((err) => console.error(err));
